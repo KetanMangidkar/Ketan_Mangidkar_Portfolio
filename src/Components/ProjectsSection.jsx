0b914148a0da4e36e3032866abc9fa9a752357dd
KetanMangidkar <ketanmangidkar@gmail.com>
2022-05-12 09:47:09 +0530

    project change alignments changes sucessfully

--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -6,8 +6,26 @@ import { SiCss3 } from "react-icons/si"
 import { SiNodedotjs } from "react-icons/si"
 import { SiExpress } from "react-icons/si"
 import { SiMongodb } from "react-icons/si"
+import { SiReact } from "react-icons/si"
 import Fade from "react-reveal/Fade"
 const projects = [
+  {
+    name: "anthropologie.com clone",
+    img: "/antrhopologie.png",
+    link: "https://anthropologie-clone.vercel.app/",
+    git: "https://github.com/Aniketkushwaha80/anthropologie-clone.git",
+    about:
+      "This is e-commerce website used for womens clothing, beauty products and home furniture and decore. This was a team project where I with my teammates had created. ",
+    stacks: [
+      <SiReact className={styles.stackIcon} />,
+      <SiNodedotjs className={styles.stackIcon} />,
+      <SiExpress className={styles.stackIcon} />,
+      <SiMongodb className={styles.stackIcon} />,
+      <SiJavascript className={styles.stackIcon} />,
+      <SiCss3 className={styles.stackIcon} />,
+      <SiHtml5 className={styles.stackIcon} />,
+    ],
+  },
   {
     name: "Revv.co.in clone",
     img: "/Revv.png",
@@ -22,6 +40,7 @@ const projects = [
       <SiMongodb className={styles.stackIcon} />,
       <SiJavascript className={styles.stackIcon} />,
       <SiCss3 className={styles.stackIcon} />,
+      <SiHtml5 className={styles.stackIcon} />,
     ],
   },
   // {
@@ -39,19 +58,19 @@ const projects = [
   //   ],
   // },
   
-  {
-    name: "Sportsjam.com clone",
-    img: "Sportsjam.png",
-    link: "sportsjam-github-io.vercel.app",
-    git: "https://github.com/KetanMangidkar/sportsjam.github.io.git",
-    about:
-    "This is e-commerce application for sports equipments. This was a solo project where I had created.",
-    stacks: [
-      <SiJavascript className={styles.stackIcon} />,
-      <SiHtml5 className={styles.stackIcon} />,
-      <SiCss3 className={styles.stackIcon} />,
-    ],
-  }
+  // {
+  //   name: "Sportsjam.com clone",
+  //   img: "Sportsjam.png",
+  //   link: "sportsjam-github-io.vercel.app",
+  //   git: "https://github.com/KetanMangidkar/sportsjam.github.io.git",
+  //   about:
+  //   "This is e-commerce application for sports equipments. This was a solo project where I had created.",
+  //   stacks: [
+  //     <SiJavascript className={styles.stackIcon} />,
+  //     <SiHtml5 className={styles.stackIcon} />,
+  //     <SiCss3 className={styles.stackIcon} />,
+  //   ],
+  // }
   
 ]
 export function ProjectsSection() {
